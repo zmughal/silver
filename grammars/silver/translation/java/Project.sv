07cfa29e584a103ebcb19025df214734223f2b1f
Kevin Williams <kwill@cs.umn.edu>
2012-01-16 15:08:59 -0600

    Issue 15: pattern variable names must start with a lower case letter and must not shadow a production name

---
+++ grammars/silver/translation/java/Project.sv
@@ -0,0 +1,8 @@
+grammar silver:translation:java;
+
+exports silver:translation:java:core;
+exports silver:translation:java:type;
+exports silver:translation:java:type:io;
+
+exports silver:translation:java:driver;
+
